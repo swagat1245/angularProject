a02062e4574bc94d88e01d685f1a3248dc438dd2
swagat1245 <swagatpatil585@gmail.com>
2023-04-23 12:07:53 +0530

    today's work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HederComponent } from './heder/heder.component';
+import { FooterComponent } from './footer/footer.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HederComponent,
+    FooterComponent,
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    EmployeeProfileComponent,
   ],
   imports: [
     BrowserModule
