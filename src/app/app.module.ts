1ef37071cc4660266dd3ff389e1c7b92fd60ee7f
swagat1245 <swagatpatil585@gmail.com>
2023-06-03 22:16:45 +0530

    todays change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-rounting.module';
 import { AppComponent } from './app.component';
-import { HederComponent } from './heder/heder.component';
-import { FooterComponent } from './footer/footer.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
-
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list /employee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HederComponent,
-    FooterComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
     EmployeeListComponent,
-    AddEmployeeComponent,
-    EmployeeProfileComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee-list /employee-list.component.spec.ts
+++ src/app/employee-list /employee-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeListComponent } from './employee-list.component';
 
@@ -6,12 +6,14 @@ describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ EmployeeListComponent ]
     })
     .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
